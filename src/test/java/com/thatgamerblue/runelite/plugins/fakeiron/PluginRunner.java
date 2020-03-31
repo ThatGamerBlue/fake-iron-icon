f9fbae55f36a5690dd838318f53fc051e6c58863
ThatGamerBlue <thatgamerblue@gmail.com>
2020-03-31 03:06:36 +0100

    yeah its somewhat right

--- src/test/java/com/thatgamerblue/runelite/plugins/fakeiron/PluginRunner.java
+++ src/test/java/com/thatgamerblue/runelite/plugins/fakeiron/PluginRunner.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.thatgamerblue.runelite.plugins.fakeiron;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluginRunner
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(FakeIronPlugin.class);
 		RuneLite.main(args);
 	}
 }
